ff5687ef5c6f4456f4f5c7175cee1f7d32ec81ad
Meet Mehta <--global>
2026-03-10 15:35:14 +0530

    cmd: wire up Pause/Resume dependencies between core and processing

--- cmd/root.go
+++ cmd/root.go
@@ -201,10 +201,30 @@ func lifecycleForLocalService(service core.DownloadService) (*processing.Lifecyc
 
 func ensureGlobalLocalServiceAndLifecycle() error {
 	if GlobalService == nil {
-		GlobalService = core.NewLocalDownloadServiceWithInput(GlobalPool, GlobalProgressCh)
+		localService := core.NewLocalDownloadServiceWithInput(GlobalPool, GlobalProgressCh)
+		GlobalService = localService
+
+		lifecycle, err := ensureLocalLifecycle(localService, currentPoolConfigs)
+		if err != nil {
+			return err
+		}
+
+		lifecycle.SetEngineHooks(processing.EngineHooks{
+			Pause:        GlobalPool.Pause,
+			Resume:       GlobalPool.Resume,
+			GetStatus:    GlobalPool.GetStatus,
+			AddConfig:    GlobalPool.Add,
+			PublishEvent: localService.Publish,
+		})
+
+		localService.PauseFunc = lifecycle.Pause
+		localService.ResumeFunc = lifecycle.Resume
+		localService.ResumeBatchFunc = lifecycle.ResumeBatch
+	} else {
+		_, err := ensureLocalLifecycle(GlobalService, currentPoolConfigs)
+		return err
 	}
-	_, err := ensureLocalLifecycle(GlobalService, currentPoolConfigs)
-	return err
+	return nil
 }
 
 func publishSystemLog(message string) {
