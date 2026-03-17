0ef53db2cd086cec1bf012900c14a5d8bc50cf19
Meet Mehta <--global>
2026-03-17 23:31:40 +0530

    feat: create new notify.go to Notify()

---
+++ internal/utils/notify.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/gen2brain/beeep"
+	"github.com/surge-downloader/surge/assets"
+)
+
+const NotificationAppName = "Surge"
+
+var iconPath string
+
+func init() {
+
+	iconPath = filepath.Join(os.TempDir(), "surge_logo.png")
+
+	err := os.WriteFile(iconPath, assets.LogoData, 0644)
+	if err != nil {
+		iconPath = ""
+		Debug("Failed to write notification icon: %v", err)
+	}
+}
+
+func Notify(title, message string) {
+
+	err := beeep.Notify(title, message, iconPath)
+	if err != nil {
+		Debug("Failed to send notification: %v", err)
+	}
+}
